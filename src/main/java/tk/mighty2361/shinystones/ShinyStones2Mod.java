74a21524296f0c811ac5cae631ee785b069452a4
xbony2 <xbony22@gmail.com>
2015-01-02 11:24:16 -0500

    all the things

---
+++ src/main/java/tk/mighty2361/shinystones/ShinyStones2Mod.java
@@ -0,0 +1,14 @@
+package tk.mighty2361.shinystones;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "ShinyStonesMod", name = "Shiny Stones 2", version = "@VERSION@")
+public class ShinyStones2Mod {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+}
